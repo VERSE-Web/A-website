b6d208d5d13805a701b87b188bccb7347e365948
Mehran Ul Islam <Verserxxx@proton.me>
2025-08-15 09:51:28 +0600

    Update Home.js

---
+++ Scripts/Home.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("btnForAlert");
+
+btn.addEventListener("click", function() {
+    alert("This is not a fully working page yet. It is still under development, so some features may not work as expected.");
+});
